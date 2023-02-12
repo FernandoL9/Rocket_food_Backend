c6b3b8bc4351bef7f453d9a7e64e2af4806d1efc
fernando lemos <fernando_lemos@hotmail.com.br>
2023-02-12 13:23:24 -0300

    add class AppError

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,6 +1,13 @@
+const AppError = require("../ultis/AppError")
+
 class UsersController {
   create(request, response) {
     const {name, email, password} = request.body
+
+    if(!name) {
+      throw new AppError("Name is require")
+    }
+
     response.status(200).json({name, email, password})
   }
 }
--- src/server.js
+++ src/server.js
@@ -1,14 +1,33 @@
+require("express-async-errors")
+
 const express = require("express"); // import express
 
 const app = express(); // starting express
 
+const AppError = require("./ultis/AppError")
+
 app.use(express.json()); // use json in message 
 
-const routes = require("./routes")
+const routes = require("./routes");
 
 const PORT = 3333; // port
 
 app.use(routes);
 
+app.use((error, request, response, next) => {
+  if(error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message
+    })
+  }
+
+  console.log(error)
+
+  return response.statusCode(500).json({
+    status: "error",
+    message: "internal server error"
+  })
+})
 
 app.listen(PORT, console.log(`Server is running on port ${PORT}`)); // testing of service server
