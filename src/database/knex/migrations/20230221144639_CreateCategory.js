2b0ab213a918e8ef5b14d5ad68043295e73bd315
fernando lemos <fernando_lemos@hotmail.com.br>
2023-02-21 18:10:05 -0300

    add migrations category e order

--- src/database/knex/migrations/20230221144639_CreateCategory.js
+++ src/database/knex/migrations/20230221144639_CreateCategory.js
@@ -2,10 +2,8 @@ exports.up = knex => knex.schema.createTable("category", table => {
   table.increments("id");
   table.text("title");
 
-
   table.integer("dishes_id").references("id").inTable("dishes")
 })
 
-exports.down = function(knex) {
-  
-};
+
+exports.down = knex => knex.schema.dropTable("category")
